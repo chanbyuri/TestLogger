9925fb471d510efd8df771366055ea37912c188a
DESKTOP-KD65D5V\TEST PC <byuri96@naver.com>
2023-03-17 17:46:07 +0900

    .

--- cui.py
+++ cui.py
@@ -1,4 +1,41 @@
+import os
+from functions.sample import *
 
 
 class MainProgram:
-    pass
+    def __init__(self):
+        self._sample_path = PROJECT_DIR+SAMPLE_FOLDER
+        self._samplefiles:list = os.listdir(self._sample_path)
+        self._sample_names:list = [os.path.splitext(i)[0] for i in self._samplefiles]
+
+        self._sample_list = {i: Sample(i) for i in self._sample_names}
+        
+    def start_test(self, sample_name):
+        self._sample_list[sample_name].add_log_now('start test')
+
+    def stop_test(self, sample_name):
+        self._sample_list[sample_name].add_log_now('stop test')
+
+    def print_samples(self):
+        print('samples',self._sample_names,end='\n')
+
+    def print_commands(self):
+        print('command list','1.start','2.stop','0.end',end='\n')
+        
+
+
+if __name__ == '__main__':
+    mp = MainProgram()
+    while True:
+        mp.print_samples()
+        mp.print_commands()
+        cmd = int(input('cmd :'))
+
+        if cmd == 0:
+            exit(0)
+        elif cmd == 1:
+            sp = str(input('sample name :'))
+            mp.start_test(sp)
+        elif cmd == 2:
+            sp = str(input('sample name :'))
+            mp.stop_test(sp)
--- frames/command_frame.py
+++ frames/command_frame.py
@@ -13,18 +13,14 @@ class CommandFrame(tk.Frame):
         self.pack(side='top', fill='x', expand=False, padx=FRAME_PAD, pady=FRAME_PAD)
 
         sample_cmd_frm = BaseCommandFrame(self, text='Sample Command')
-        # sample_cmd_frm.pack(side='left', padx=FRAME_PAD, pady=FRAME_PAD)
 
         def new_sample():
-            # new = tk.messagebox.askokcancel()
             tl = tk.Toplevel(self)
             tl.resizable(False, False)
             tl.title("setup new sample")
             tl.attributes('-topmost', 'true')
 
         btn_new_sample = LeftSideButton(sample_cmd_frm, text='New Sample', command=new_sample)
-        # btn_new_sample = tk.Button(sample_cmd_frm, text='New Sample', command=new_sample)
-        # btn_new_sample.pack(side='left', padx=CHILD_PAD, pady=CHILD_PAD)
 
         def pop_sample():
             tl = tk.Toplevel(self)
@@ -33,28 +29,22 @@ class CommandFrame(tk.Frame):
             tl.attributes('-topmost', 'true')
 
         btn_pop_sample = LeftSideButton(sample_cmd_frm, text='Pop Sample', command=pop_sample)
-        # btn_pop_sample.pack(side='left', padx=CHILD_PAD, pady=CHILD_PAD)
 
         total_cmd_frm = BaseCommandFrame(self, text='Total Command')
-        # total_cmd_frm.pack(side='left', padx=FRAME_PAD, pady=FRAME_PAD)
 
         def start_all():
             tk.messagebox.showinfo('start all', 'All Sample Run Now.')
             pass
 
         btn_start = LeftSideButton(total_cmd_frm, text='Start All', command=start_all)
-        # btn_start = tk.Button(total_cmd_frm, text='Start All', command=start_all)
-        # btn_start.pack(side='left', padx=CHILD_PAD, pady=CHILD_PAD)
 
         def stop_all():
             tk.messagebox.showinfo('stop all', 'All Sample Stop Now.')
             pass
 
         btn_stop = LeftSideButton(total_cmd_frm, text='Stop All', command=stop_all)
-        # btn_stop.pack(side='left', padx=CHILD_PAD, pady=CHILD_PAD)
 
         ext_frm = BaseCommandFrame(self, text='Ext Command')
-        # ext_frm.pack(side='left', padx=FRAME_PAD, pady=FRAME_PAD)
 
         def quit_program():
             tk.messagebox.showinfo('quit', 'quit this program.')
@@ -62,7 +52,6 @@ class CommandFrame(tk.Frame):
             pass
 
         btn_quit = LeftSideButton(ext_frm, text='Quit', command=quit_program)
-        # btn_quit.pack(side='left', padx=CHILD_PAD, pady=CHILD_PAD)
 
         def shutdown_machine():
             tk.messagebox.showinfo('shut down', 'shut down only raspberrypi.')
@@ -70,10 +59,6 @@ class CommandFrame(tk.Frame):
                 os.system('sudo shutdown -h now')
 
         btn_shutdown = LeftSideButton(ext_frm, text='Shut Down', command=shutdown_machine)
-        # btn_shutdown.pack(side='left', padx=CHILD_PAD, pady=CHILD_PAD)
-
-        # btn_start_all = tk.Button(self, text='Start')
-        # btn_start_all.pack(side='left', padx=CHILD_PAD, pady=CHILD_PAD)
 
 
 class BaseCommandFrame(tk.LabelFrame):
